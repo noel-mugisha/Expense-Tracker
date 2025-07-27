c155f6e215032c4f0284adf92147016a256ed4e6
Noel <noelmugisha332@gmail.com>
2025-07-27 13:11:43 +0200

    Added new Funtionalities for the service

--- src/main/java/com/noel/service/ExpenseService.java
+++ src/main/java/com/noel/service/ExpenseService.java
@@ -3,7 +3,10 @@ package com.noel.service;
 import com.noel.model.Expense;
 import com.noel.repository.ExpenseRepository;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ExpenseService {
     private final ExpenseRepository expenseRepository = new ExpenseRepository();
@@ -21,6 +24,21 @@ public class ExpenseService {
     public void getAllExpenses() {
         List<Expense> allExpenses = expenseRepository.getExpenses();
         allExpenses.forEach(System.out::println);
+    }
+
+    public void getMonthlyTotals() {
+        Map<String, Double> monthlyTotals = expenseRepository.getExpenses().stream()
+                .collect(Collectors.groupingBy( expense -> expense.getDate().getMonth().name(),
+                        Collectors.summingDouble(Expense::getAmount)));
+
+        monthlyTotals.forEach((key, value) -> System.out.println(key + " --> " + value + " RWF"));
+    }
+
+    public void getCategoryWiseTotals() {
+        Map<String, Double> categoryTotals = expenseRepository.getExpenses().stream()
+                .collect(Collectors.groupingBy(Expense::getCategory, Collectors.summingDouble(Expense::getAmount)));
+
+        categoryTotals.forEach((key, value) -> System.out.println(key + " --> " + value + " RWF"));
     }
 
 }
