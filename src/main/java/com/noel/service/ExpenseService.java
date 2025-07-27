a95593be51ae3f68f543a3028156d04dc6182b04
Noel <noelmugisha332@gmail.com>
2025-07-27 13:42:56 +0200

    Final touches on service layer

--- src/main/java/com/noel/service/ExpenseService.java
+++ src/main/java/com/noel/service/ExpenseService.java
@@ -3,6 +3,8 @@ package com.noel.service;
 import com.noel.model.Expense;
 import com.noel.repository.ExpenseRepository;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -39,6 +41,34 @@ public class ExpenseService {
                 .collect(Collectors.groupingBy(Expense::getCategory, Collectors.summingDouble(Expense::getAmount)));
 
         categoryTotals.forEach((key, value) -> System.out.println(key + " --> " + value + " RWF"));
+    }
+
+    public void generateReport () throws IOException {
+        List<Expense> expenses = expenseRepository.getExpenses();
+        Map<String, Double> monthlyTotals = expenseRepository.getExpenses().stream()
+                .collect(Collectors.groupingBy( expense -> expense.getDate().getMonth().name(),
+                        Collectors.summingDouble(Expense::getAmount)));
+        Map<String, Double> categoryTotals = expenseRepository.getExpenses().stream()
+                .collect(Collectors.groupingBy(Expense::getCategory, Collectors.summingDouble(Expense::getAmount)));
+
+        try(PrintWriter  writer = new PrintWriter("REPORT.txt")) {
+            // Write monthly totals
+            writer.println("=== MONTHLY TOTALS ===");
+            monthlyTotals.forEach((month, amount) ->
+                    writer.printf("%s --> %.2f RWF%n", month, amount));
+
+            // Write category-wise totals
+            writer.println("\n=== CATEGORY TOTALS ===");
+            categoryTotals.forEach((category, amount) ->
+                    writer.printf("%s --> %.2f RWF%n", category, amount));
+
+            // write all expenses report
+            writer.println("\n=== ALL EXPENSES ===");
+            expenses.forEach(System.out::println);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
